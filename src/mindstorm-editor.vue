039078b3d43d928fc7ee20aeca0a56737e3448f3
Jason Ford <fordjason@gmail.com>
2025-01-29 15:05:51 -0800

    Add box to upload into

--- src/mindstorm-editor.vue
+++ src/mindstorm-editor.vue
@@ -1,12 +1,40 @@
 <script setup>
   const props = defineProps({
-  	uuid: String
+    uuid: String
   })
 
   const mindstorm = await Agent.state(props.uuid)
 </script>
 
 <template>
-  {{ uuid }}
-  {{ mindstorm }}
+  <div id="mindstorm-editor">
+    <svg viewBox="0 0 100 100" preserveAspectRatio="xMidYMid meet">
+      <rect
+        x="0"
+        y="0"
+        width="100"
+        height="100"
+        fill="none"
+        stroke-width="1"
+        stroke="black"
+        rx="2"
+      />
+    </svg>
+  </div>
 </template>
+
+<style>
+  #mindstorm-editor {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  #mindstorm-editor svg {
+    width: 90%;
+    height: 90%;
+    display: block;
+    overflow: visible;
+  }
+</style>

65e4c82f5ba8e451a3feee125552e714fde1e68f
Jason Ford <fordjason@gmail.com>
2025-02-04 11:31:48 -0800

    Start modularizing world

--- src/player.vue
+++ src/player.vue
@@ -1,18 +1,150 @@
 <script setup>
   import World from './world.vue'
+  import { ref, reactive } from 'vue'
+  import { useAnimationLoop } from './animation-loop.js'
   import { useKeyboardEvents } from './keyboard.js'
+
+  const { registerKey } = useKeyboardEvents()
+  const { registerAnimationCallback }  = useAnimationLoop()
 
   const props = defineProps({ uuid: String })
 
-  const { registerKey } = useKeyboardEvents()
-
   registerKey('Escape', () => Agent.close())
+
+  const mindstorm = JSON.parse(JSON.stringify(await Agent.state(props.uuid)))
+  const world = reactive(await Agent.state(`run-state/${props.uuid}`))
+
+  function checkCollision(rect1, rect2) {
+    return (
+        rect1.x < rect2.x + rect2.width &&
+        rect1.x + rect1.width > rect2.x &&
+        rect1.y < rect2.y + rect2.height &&
+        rect1.y + rect1.height > rect2.y
+    )
+  }
+
+  const initialized = new Set()
+
+  registerAnimationCallback(() => {
+    Object
+      .entries(world)
+      .forEach(([name1, object]) => {
+        let { dimensions: d1, handlers, collisions } = object
+        if (!initialized.has(object)) {
+          initialized.add(object)
+          if (handlers?.initialize) {
+            try {
+              const event = {}
+
+              const scopedHandler = new Function('world', 'event', `with (world, event) { ${handlers.initialize} }`)
+              const result = scopedHandler.bind(world[name1])(world, event)
+            }
+            catch (error) {
+              console.error('Error:', error.message)
+            }
+          }
+        }
+        if (handlers?.step) {
+          try {
+            const event = {}
+
+            const scopedHandler = new Function('world', 'event', `with (world, event) { ${handlers.step} }`)
+            const result = scopedHandler.bind(world[name1])(world, event)
+          }
+          catch (error) {
+            console.error('Error:', error.message)
+          }
+        }
+
+        const collisionsSeen = {}
+
+        if (!collisions) {
+          world[name1].collisions = {}
+          collisions = world[name1].collisions
+        }
+
+        Object
+          .entries(world)
+          .forEach(([name2, { dimensions: d2 }]) => {
+            if (name1 === name2) return
+
+            if (handlers?.collide && checkCollision(d1, d2)) {
+              const handler = handlers.collide
+              collisionsSeen[name2] = true
+              if (collisions[name2]) return
+
+              collisions[name2] = { someCollisionData: false }
+              try {
+                const event = { other: name2 }
+
+                const scopedHandler = new Function('world', 'event', `with (world, event) { ${handler} }`)
+                const result = scopedHandler.bind(world[name])(world, event)
+              }
+              catch (error) {
+                console.error('Error:', error.message)
+              }
+            }
+          })
+        Object
+          .keys(collisions)
+          .forEach(name => {
+            if (!collisionsSeen[name]) {
+              delete collisions[name]
+              if (handlers?.uncollide) {
+                const handler = handlers.uncollide
+                try {
+                  const event = { other: name }
+                  const scopedHandler = new Function('world', 'event', `with (world, event) { ${handler} }`)
+                  const result = scopedHandler.bind(world[name])(world, event)
+                }
+                catch (error) {
+                  console.error('Error:', error.message)
+                }
+              }
+            }
+          })
+      })
+  })
+
+  Object.keys(world).forEach(key => delete world[key])
+  Object.assign(world, mindstorm)
+
+  function handleClick({target: name, event: rawEvent}) {
+    const handler = world?.[name]?.handlers?.click
+    if (handler) {
+      try {
+        const event = {}
+        const scopedHandler = new Function('world', 'event', `with (world, event) { ${handler} }`)
+        const result = scopedHandler.bind(world[name])(world, event)
+      } catch (error) {
+        console.error('Error:', error.message)
+      }
+    }
+  }
+
+  function handleDrag({target: name, event }) {
+    const { detail: { svg_x:x, svg_y:y, svg_dx:dx, svg_dy:dy } } = event
+    const handler = world?.[name]?.handlers?.drag
+    if (handler) {
+      try {
+        const event = { x, y, dx, dy }
+        const scopedHandler = new Function('world', 'event', `with (world, event) { ${handler} }`)
+        const result = scopedHandler.bind(world[name])(world, event)
+      } catch (error) {
+        console.error('Error:', error.message)
+      }
+    }
+  }
 
 </script>
 
 <template>
   <div id="mindstorm-player">
-    <World :uuid="uuid" />
+    <World
+      :world="world"
+      @drag="handleDrag"
+      @click="handleClick"
+    />
   </div>
 </template>
 
--- src/world.vue
+++ src/world.vue
@@ -1,143 +1,13 @@
 <script setup>
-  import { ref, reactive } from 'vue'
   import Image from './image.vue'
-  import { useAnimationLoop } from './animation-loop.js'
 
-  const props = defineProps({ uuid: String })
+  defineProps({ world: Object })
+  const emit = defineEmits(['click', 'drag'])
 
-  const mindstorm = JSON.parse(JSON.stringify(await Agent.state(props.uuid)))
-  const world = reactive(await Agent.state(`run-state/${props.uuid}`))
-  const svg = ref(null)
-
-  const { registerAnimationCallback }  = useAnimationLoop()
-
-  function checkCollision(rect1, rect2) {
-    return (
-        rect1.x < rect2.x + rect2.width &&
-        rect1.x + rect1.width > rect2.x &&
-        rect1.y < rect2.y + rect2.height &&
-        rect1.y + rect1.height > rect2.y
-    )
-  }
-
-  const initialized = new Set()
-
-  registerAnimationCallback(() => {
-    Object
-      .entries(world)
-      .forEach(([name1, object]) => {
-        let { dimensions: d1, handlers, collisions } = object
-        if (!initialized.has(object)) {
-          initialized.add(object)
-          if (handlers?.initialize) {
-            try {
-              const event = {}
-
-              const scopedHandler = new Function('world', 'event', `with (world, event) { ${handlers.initialize} }`)
-              const result = scopedHandler.bind(world[name1])(world, event)
-            }
-            catch (error) {
-              console.error('Error:', error.message)
-            }
-          }
-        }
-        if (handlers?.step) {
-          try {
-            const event = {}
-
-            const scopedHandler = new Function('world', 'event', `with (world, event) { ${handlers.step} }`)
-            const result = scopedHandler.bind(world[name1])(world, event)
-          }
-          catch (error) {
-            console.error('Error:', error.message)
-          }
-        }
-
-        const collisionsSeen = {}
-
-        if (!collisions) {
-          world[name1].collisions = {}
-          collisions = world[name1].collisions
-        }
-
-        Object
-          .entries(world)
-          .forEach(([name2, { dimensions: d2 }]) => {
-            if (name1 === name2) return
-
-            if (handlers?.collide && checkCollision(d1, d2)) {
-              const handler = handlers.collide
-              collisionsSeen[name2] = true
-              if (collisions[name2]) return
-
-              collisions[name2] = { someCollisionData: false }
-              try {
-                const event = { other: name2 }
-
-                const scopedHandler = new Function('world', 'event', `with (world, event) { ${handler} }`)
-                const result = scopedHandler.bind(world[name])(world, event)
-              }
-              catch (error) {
-                console.error('Error:', error.message)
-              }
-            }
-          })
-        Object
-          .keys(collisions)
-          .forEach(name => {
-            if (!collisionsSeen[name]) {
-              delete collisions[name]
-              if (handlers?.uncollide) {
-                const handler = handlers.uncollide
-                try {
-                  const event = { other: name }
-                  const scopedHandler = new Function('world', 'event', `with (world, event) { ${handler} }`)
-                  const result = scopedHandler.bind(world[name])(world, event)
-                }
-                catch (error) {
-                  console.error('Error:', error.message)
-                }
-              }
-            }
-          })
-      })
-  })
-
-  Object.keys(world).forEach(key => delete world[key])
-  Object.assign(world, mindstorm)
-
-  function handleClick(name, rawEvent) {
-    const handler = world?.[name]?.handlers?.click
-    if (handler) {
-      try {
-        const event = {}
-        const scopedHandler = new Function('world', 'event', `with (world, event) { ${handler} }`)
-        const result = scopedHandler.bind(world[name])(world, event)
-      } catch (error) {
-        console.error('Error:', error.message)
-      }
-    }
-  }
-
-  function handleDrag(name, event) {
-    const { detail: { svg_x:x, svg_y:y, svg_dx:dx, svg_dy:dy } } = event
-    console.log('DRAG EVENT', event)
-    const handler = world?.[name]?.handlers?.drag
-    if (handler) {
-      try {
-        const event = { x, y, dx, dy }
-        const scopedHandler = new Function('world', 'event', `with (world, event) { ${handler} }`)
-        const result = scopedHandler.bind(world[name])(world, event)
-      } catch (error) {
-        console.error('Error:', error.message)
-      }
-    }
-  }
 </script>
 
 <template>
   <svg
-    ref="svg"
     viewBox="0 0 100 100"
     preserveAspectRatio="xMidYMid meet"
   >
@@ -164,8 +34,8 @@
         @mousedown.stop="selected = name"
         :transform="`translate(${dimensions.x}, ${dimensions.y}) rotate(${angle}, ${origin.x}, ${origin.y})`"
         v-drag
-        @drag="event => handleDrag(name, event)"
-        @click="event => handleClick(name, event)"
+        @drag="event => emit('drag', { target: name, event })"
+        @click="event => emit('click', { target: name, event })"
       >
         <Image
           v-if="sprite?.sheet"
